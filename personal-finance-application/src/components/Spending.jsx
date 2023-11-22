1d409f02e5bbeea6f4b3c4379afbe3b969f5c0b2
Gabriel Johnson <johnsga2@oregonstate.edu>
2023-11-21 17:25:24 -0800

    table started

--- personal-finance-application/src/components/Spending.jsx
+++ personal-finance-application/src/components/Spending.jsx
@@ -1,13 +1,38 @@
 import React, { useState } from "react";
 import NavBar from "./NavBar";
+import { SpendingTable } from "./SpendingTable";
 
 export const Spending = (props) => {
 
+    const rows = [];
+    let numrows=5;
+    for (let i = 0; i < numrows; i++) {
+        // note: we are adding a key prop here to allow react to uniquely identify each
+        // element in this array. see: https://reactjs.org/docs/lists-and-keys.html
+        rows.push(<SpendingTable key={i} />);
+    }
 
     return(
         <div>
             <NavBar/>
-            <div>spending page!</div>
+            <div class="input-spending-container">
+                <h2>Input Spending Data</h2>
+            </div>
+            <div class="main-table">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Name?</th>
+                            <th>Amount</th>
+                            <th>Date</th>
+                            {/* <th>Edit</  th> */}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {rows}
+                    </tbody>
+                </table>
+            </div>
         </div>
     )
 
---
+++ personal-finance-application/src/components/SpendingTable.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import NavBar from "./NavBar";
+
+export const SpendingTable = (props) => {
+
+
+    return(
+        
+        <tr>
+            <td>Data 1</td>
+            <td>Data 2</td>
+            <td>Data 3</td>
+            <button>Edit</button>
+
+        </tr>
+         
+    )
+
+
+
+}
