4804f4a1dd166d4605bb7902dc30c33ec19e8495
tankit03 <huparetanish@gmail.com>
2023-11-28 14:55:19 -0800

    commiting before pulling main

--- personal-finance-application/src/components/Graph/GraphPage.jsx
+++ personal-finance-application/src/components/Graph/GraphPage.jsx
@@ -1,53 +1,65 @@
-import React, { useState } from "react";
-import "./Graph.css"
+import React, { useEffect, useRef } from "react";
 import NavBar from "../NavBar";
-import { PieChart, Pie, Tooltip } from "recharts";
+import { Chart } from 'chart.js/auto';
+import './Graph.css';
 
 const GraphApp = () => {
+  const chartRef = useRef(null);
 
-    const data = [
+  const getRandomColor = () => {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  };
 
-        {name: "Utilities", value: 300},
-        {name: "Rent", value: 1200},
-        {name: "Groceries", value: 600},
-        {name: "Insurance", value: 300},
-        {name: "Insurance", value: 300},
-        {name: "subscription", value: 230},
+  const data = [
+    { name: "Utilities", value: 300 },
+    { name: "Rent", value: 1200, color: "#2196f3" },
+    { name: "Groceries", value: 600 },
+    { name: "Insurance", value: 300 },
+    { name: "Insurance", value: 300 },
+    { name: "subscription", value: 230 },
+  ];
 
+  useEffect(() => {
+    const ctx = chartRef.current.getContext("2d");
 
-    ];
-    
+    // Destroy the previous Chart instance if it exists
+    if (chartRef.current.chart) {
+      chartRef.current.chart.destroy();
+    }
 
-    return(
-        
-        <div className="Graph">
-            <h1>Monthly Expenses</h1>
-            <div className="GraphContainer">
-                    <PieChart width={400} height={400}>
-                <Pie
-                    dataKey="value"
-                    isAnimationActive={false}
-                    data={data}
-                    cx="50%"
-                    cy="50%"
-                    outerRadius={80}
-                    fill="##4caf50"
-                    label
-                />
-                <Pie dataKey="value" data={data} cx={500} cy={200} innerRadius={40} outerRadius={80} fill="#82ca9d" />
-                <Tooltip />
-                </PieChart>
-                
-                <ul className="ExpenseList">
-                    {data.map((entry) => (
-                        <li key={entry.name}>
-                            {entry.name}: {entry.value}
-                        </li>
-                    ))}
-                </ul>
+    // Create a new Chart instance
+    chartRef.current.chart = new Chart(ctx, {
+      type: "pie",
+      data: {
+        labels: data.map(item => item.name),
+        datasets: [
+          {
+            data: data.map(item => item.value),
+            backgroundColor: data.map(item => item.color || getRandomColor()),
+          },
+        ],
+      },
+    });
+  }, [data]);
+
+  return (
+    <div>
+      {/* Include NavBar component */}
+      <NavBar />
+
+      <div className="Graph">
+        <h1>Monthly Expenses</h1>
+        <div className="GraphContainer">
+          <canvas ref={chartRef} width="400" height="400" />
         </div>
+      </div>
     </div>
-    )
+  );
 };
 
 export default GraphApp;
