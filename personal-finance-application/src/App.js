0d2287b912a44f2f920696142f13d8d5fe0e845b
Alex Giger <57809929+giiger@users.noreply.github.com>
2023-11-28 17:17:25 -0800

    dark mode settings page

--- personal-finance-application/src/App.js
+++ personal-finance-application/src/App.js
@@ -5,10 +5,27 @@ import {LandingPage} from './components/LandingPage'
 import {LoginPage} from './components/LoginPage';
 import {RegisterPage} from './components/RegisterPage';
 import SettingsPage from './components/SettingsPage';
+<<<<<<< Updated upstream
 
 function App() {
   const [currentForm, setCurrentForm] = useState('login');
   const [submitted, setSubmitted] = useState(false);
+=======
+import {Spending} from './components/Spending';
+import  GraphPage  from "./components/Graph/GraphPage";
+import { ThemeContext } from './ThemeContext';
+
+function App() {
+const [currentForm, setCurrentForm] = useState('login');
+const [submitted, setSubmitted] = useState(false);
+const [theme, setTheme] = useState('light');
+
+
+
+const toggleForm = (formName) => {
+setCurrentForm(formName);
+}
+>>>>>>> Stashed changes
 
   const toggleForm = (formName) => {
     setCurrentForm(formName);
@@ -18,8 +35,12 @@ function App() {
     setSubmitted(true);
     console.log(submitted);
   }
+  const toggleTheme = () => {
+    setTheme(theme === 'light' ? 'dark' : 'light');
+  }
 
   return (
+<<<<<<< Updated upstream
     <div className="App">
       <Router>
         {submitted ? (
@@ -35,6 +56,27 @@ function App() {
         )}
       </Router>
     </div>
+=======
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <div className={`App ${theme}`}>
+        <Router>
+          {submitted ? (
+            <Routes>
+              <Route path="/" element={<LandingPage />} />
+              <Route path="/settings" element={<SettingsPage />} />
+              <Route path="/spending" element={<Spending />} />
+              <Route path="/GraphPage" element={<GraphPage />} />
+            </Routes>
+          ) : (
+            currentForm === "login" ? 
+              <LoginPage onFormSwitch={toggleForm} onSubmit={handleFormSubmit}/> 
+              : 
+              <RegisterPage onFormSwitch={toggleForm} onSubmit={handleFormSubmit}/>
+          )}
+        </Router>
+      </div>
+    </ThemeContext.Provider>
+>>>>>>> Stashed changes
   );
 }
 
--- personal-finance-application/src/components/SettingsPage.jsx
+++ personal-finance-application/src/components/SettingsPage.jsx
@@ -1,7 +1,9 @@
-
-import React from 'react';
+import { ThemeContext } from '../ThemeContext';
+import React, {useContext} from 'react';
 
 const SettingsPage = () => {
+  const { toggleTheme } = useContext(ThemeContext);
+  
   return (
     <div>
       <h1>Settings</h1>
@@ -16,6 +18,8 @@ const SettingsPage = () => {
         <input type="password" id="password" name="password" />
 
         <button type="submit">Save</button>
+
+        <button type="button" onClick={toggleTheme}>Toggle Dark Mode</button>
       </form>
     </div>
   );
