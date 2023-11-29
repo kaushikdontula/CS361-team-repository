0521b239a8db07fe4b58f0ed6ac10483bba742a1
Kaushik Dontula <kaushikdontula@gmail.com>
2023-11-28 17:16:36 -0800

    Local Storage Objects Working

--- personal-finance-application/src/components/Spending.jsx
+++ personal-finance-application/src/components/Spending.jsx
@@ -1,110 +1,143 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import NavBar from "./NavBar";
 import { SpendingTable } from "./SpendingTable";
 
 export const Spending = (props) => {
+  const localStorageKey = "transactionsData";
 
-    //set transactiond data
-    var transactionData= useState({
-        name: "",
-        amount: 0,
-        date: "",
-        category: "",
-    });
-    
-    //state to manage rows of transation data
-    const [rows, setRows] = useState([]);
-    
-    //adding a transaction to the table
-    const addTransactionToTable = () => {
-        setRows((prevRows) => [
-        ...prevRows,
-        <SpendingTable key={prevRows.length} data={transactionData} />,
-        ]);
-        closeModal();
-    };
+  // state to manage rows of transaction data
+  const [rows, setRows] = useState([]);
 
-    //state for managing model
-    const [isModalOpen, setModalOpen] = useState(false);
+  // state for managing model
+  const [isModalOpen, setModalOpen] = useState(false);
 
-    const openModal = () => {
-        setModalOpen(true);
-    };
+  // state to manage transaction input data
+  const [transactionData, setTransactionData] = useState({
+    name: "",
+    amount: 0,
+    date: "",
+    category: "",
+  });
 
-    const closeModal = () => {
-        setModalOpen(false);
-    };
+  // state to manage array of transaction objects
+  const [transactionsData, setTransactionsData] = useState([]);
 
-    function setTransactionData (data, arg) {
-      transactionData = data;
-    };
+  useEffect(() => {
+    // Load transactions data from local storage on component mount
+    const storedTransactions = JSON.parse(localStorage.getItem(localStorageKey));
+    if (storedTransactions) {
+      setTransactionsData(storedTransactions);
+      setRows(
+        storedTransactions.map((transaction, index) => (
+          <SpendingTable key={index} data={transaction} />
+        ))
+      );
+    }
+  }, []);
 
-    //modal component to input transaction data
-    const Modal = ({ isOpen, onClose }) => {
-        return (
-        <>
-            {isOpen && (
-            <div className="overlay" onClick={onClose}>
-                <div className="modal" onClick={(e) => e.stopPropagation()}>
-                <span className="close" onClick={onClose}>
-                    &times;
-                </span>
-                <h2>Input Data</h2>
+  // adding a transaction to the table
+  const addTransactionToTable = () => {
+    // create a new transaction object
+    const newTransaction = { ...transactionData };
 
-                    {/* name input */}
-                    <label htmlFor="name">Name</label>
-                    <input
-                    type="text"
-                    id="Name"
-                    value={transactionData.name}
-                    onChange={(e) =>
-                        setTransactionData({ ...transactionData, name: e.target.value })
-                    }
-                    />
+    // add the transaction object to the array of transactions
+    setTransactionsData((prevTransactions) => [...prevTransactions, newTransaction]);
 
-                    {/* amount input */}
-                    <label htmlFor="amount">Amount</label>
-                    <input
-                    type="number"
-                    id="amount"
-                    value={transactionData.amount}
-                    onChange={(e) =>
-                        setTransactionData({
-                        ...transactionData,
-                        amount: e.target.value !== "" ? Number(e.target.value) : "",
-                        })
-                    }
-                    />
+    // update the rows for the table
+    setRows((prevRows) => [
+      ...prevRows,
+      <SpendingTable key={prevRows.length} data={newTransaction} />,
+    ]);
 
-                    {/* date input */}
-                    <label htmlFor="date">Date</label>
-                    <input
-                        type="date"
-                        id="date"
-                        value={transactionData.date}
-                        onChange={(e) =>
-                        setTransactionData({ ...transactionData, date: e.target.value })
-                        }
-                    />
+    // update local storage
+    localStorage.setItem(localStorageKey, JSON.stringify([...transactionsData, newTransaction]));
 
-                    {/* category input */}
-                    <label htmlFor="Category">Category</label>
-                    <input
-                    type="text"
-                    id="Category"
-                    value={transactionData.category}
-                    onChange={(e) =>
-                        setTransactionData({ ...transactionData, category: e.target.value })
-                    }
-                    />
+    closeModal();
+  };
 
-                <button onClick={addTransactionToTable}>Add Transaction</button>
-                </div>
+  const openModal = () => {
+    setModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
+  // modal component to input transaction data
+  const Modal = ({ isOpen, onClose }) => {
+    return (
+      <>
+        {isOpen && (
+          <div className="overlay" onClick={onClose}>
+            <div className="modal" onClick={(e) => e.stopPropagation()}>
+              <span className="close" onClick={onClose}>
+                &times;
+              </span>
+              <h2>Input Data</h2>
+
+              {/* name input */}
+              <label htmlFor="name">Name</label>
+              <input
+                type="text"
+                id="Name"
+                value={transactionData.name}
+                onChange={(e) =>
+                  setTransactionData({
+                    ...transactionData,
+                    name: e.target.value,
+                  })
+                }
+              />
+
+              {/* amount input */}
+              <label htmlFor="amount">Amount</label>
+              <input
+                type="number"
+                id="amount"
+                value={transactionData.amount}
+                onChange={(e) =>
+                  setTransactionData({
+                    ...transactionData,
+                    amount: e.target.value !== "" ? Number(e.target.value) : 0,
+                  })
+                }
+              />
+
+              {/* date input */}
+              <label htmlFor="date">Date</label>
+              <input
+                type="date"
+                id="date"
+                value={transactionData.date}
+                onChange={(e) =>
+                  setTransactionData({
+                    ...transactionData,
+                    date: e.target.value,
+                  })
+                }
+              />
+
+              {/* category input */}
+              <label htmlFor="Category">Category</label>
+              <input
+                type="text"
+                id="Category"
+                value={transactionData.category}
+                onChange={(e) =>
+                  setTransactionData({
+                    ...transactionData,
+                    category: e.target.value,
+                  })
+                }
+              />
+
+              <button onClick={addTransactionToTable}>Add Transaction</button>
             </div>
-            )}
-        </>
-        );
-    };
+          </div>
+        )}
+      </>
+    );
+  };
 
   return (
     <div>
@@ -134,7 +167,4 @@ export const Spending = (props) => {
       </div>
     </div>
   );
-
-
-
-}
+};
