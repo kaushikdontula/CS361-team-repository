8c2319cabeaf3329cea5ad0aba1de32d1d908f82
tankit03 <huparetanish@gmail.com>
2023-11-28 17:25:15 -0800

    added drop down - tanish

--- personal-finance-application/src/components/Graph/GraphPage.jsx
+++ personal-finance-application/src/components/Graph/GraphPage.jsx
@@ -1,10 +1,12 @@
-import React, { useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import NavBar from "../NavBar";
 import { Chart } from 'chart.js/auto';
 import './Graph.css';
 
 const GraphApp = () => {
   const chartRef = useRef(null);
+  const [selectedDate, setSelectedDate] = useState("All"); // Default to show all dates
+  const [selectedCategory, setSelectedCategory] = useState("All"); // Default to show all categories
 
   const getRandomColor = () => {
     const letters = "0123456789ABCDEF";
@@ -16,13 +18,25 @@ const GraphApp = () => {
   };
 
   const data = [
-    { name: "Utilities", value: 300 },
-    { name: "Rent", value: 1200, color: "#2196f3" },
-    { name: "Groceries", value: 600 },
-    { name: "Insurance", value: 300 },
-    { name: "Insurance", value: 300 },
-    { name: "subscription", value: 230 },
+    { 
+      name: "Rent",
+      value: 300, 
+      date: "2023-11-29",
+      category: "Rent"
+    },
+    { 
+      name: "Rent",
+      value: 400, 
+      date: "2023-12-29",
+      category: "Rent"
+    },
+    { name: "Subscription", value: 230, date: "2023-11-30", category: "Subscription" },
+    { name: "Groceries", value: 150, date: "2023-11-30", category: "Groceries" },
+    // Add more data entries as needed
   ];
+
+  const uniqueDates = [...new Set(data.map(item => item.date))];
+  const uniqueCategories = [...new Set(data.map(item => item.category))];
 
   useEffect(() => {
     const ctx = chartRef.current.getContext("2d");
@@ -32,20 +46,28 @@ const GraphApp = () => {
       chartRef.current.chart.destroy();
     }
 
+    // Filter data based on selected date and category
+    const filteredData = data.filter(item => {
+      return (
+        (selectedDate === "All" || item.date === selectedDate) &&
+        (selectedCategory === "All" || item.category === selectedCategory)
+      );
+    });
+
     // Create a new Chart instance
     chartRef.current.chart = new Chart(ctx, {
       type: "pie",
       data: {
-        labels: data.map(item => item.name),
+        labels: filteredData.map(item => item.name),
         datasets: [
           {
-            data: data.map(item => item.value),
-            backgroundColor: data.map(item => item.color || getRandomColor()),
+            data: filteredData.map(item => item.value),
+            backgroundColor: filteredData.map(item => item.color || getRandomColor()),
           },
         ],
       },
     });
-  }, [data]);
+  }, [data, selectedDate, selectedCategory]);
 
   return (
     <div>
@@ -54,6 +76,37 @@ const GraphApp = () => {
 
       <div className="Graph">
         <h1>Monthly Expenses</h1>
+
+        {/* Dropdown for Date */}
+        <label htmlFor="dateDropdown">Select Date: </label>
+        <select
+          id="dateDropdown"
+          onChange={(e) => setSelectedDate(e.target.value)}
+          value={selectedDate}
+        >
+          <option value="All">All</option>
+          {uniqueDates.map((date) => (
+            <option key={date} value={date}>
+              {date}
+            </option>
+          ))}
+        </select>
+
+        {/* Dropdown for Category */}
+        <label htmlFor="categoryDropdown">Select Category: </label>
+        <select
+          id="categoryDropdown"
+          onChange={(e) => setSelectedCategory(e.target.value)}
+          value={selectedCategory}
+        >
+          <option value="All">All</option>
+          {uniqueCategories.map((category) => (
+            <option key={category} value={category}>
+              {category}
+            </option>
+          ))}
+        </select>
+
         <div className="GraphContainer">
           <canvas ref={chartRef} width="400" height="400" />
         </div>
