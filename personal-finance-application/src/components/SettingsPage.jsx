15555d6258d2835448d25127ed8acae8c064e494
Alex Giger <57809929+giiger@users.noreply.github.com>
2023-12-02 19:35:01 -0800

    added more buttons

--- personal-finance-application/src/components/SettingsPage.jsx
+++ personal-finance-application/src/components/SettingsPage.jsx
@@ -1,12 +1,19 @@
 import { ThemeContext } from '../ThemeContext';
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, } from 'react';
 import NavBar from "./NavBar";
+import {useNavigate} from 'react-router-dom';
 
 const SettingsPage = () => {
   const { toggleTheme } = useContext(ThemeContext);
   const [lastSavedTime, setLastSavedTime] = useState(null);
   const [cardName, setCardName] = useState('');
   const [showCardName, setShowCardName] = useState(false);
+  const [deleteConfirmation, setDeleteConfirmation] = useState(false);
+
+  const navigate = useNavigate();
+  const handleEditSpendingData = () => {
+    navigate("/editSpending");
+  };
 
   const handleSaveData = () => {
     if (navigator.onLine) {
@@ -25,11 +32,28 @@ const SettingsPage = () => {
 
   const handleCardNameChange = (event) => {
     setCardName(event.target.value);
-    localStorage.setItem('cardName', cardName);
+    localStorage.setItem('cardName', event.target.value);
   };
 
   const handleAddSpendingMethod = () => {
     setShowCardName(!showCardName);
+  };
+
+  const handleSubmitCardName = () => {
+    // Handle card name submission here
+    alert("Card name submitted: " + cardName);
+    setShowCardName(!showCardName);
+  };
+
+  const handleDeleteAccount = () => {
+    if (deleteConfirmation) {
+      // Delete the user's account here
+      localStorage.clear();
+      alert("User account deleted successfully");
+      window.location.href = "/";
+    } else {
+      setDeleteConfirmation(true);
+    }
   };
 
   return (
@@ -42,27 +66,39 @@ const SettingsPage = () => {
         </h1>
         <h1>
           {showCardName ? (
-            <input
-              type="text"
-              value={cardName}
-              onChange={handleCardNameChange}
-              placeholder="Enter card name"
-              style={{
-                padding: '10px',
-                fontSize: '16px',
-                border: '1px solid #ccc',
-                borderRadius: '4px',
-                width: '300px'
-              }}
-            />
+            <div>
+              <input
+                type="text"
+                value={cardName}
+                onChange={handleCardNameChange}
+                placeholder="Enter card name"
+                style={{
+                  padding: '10px',
+                  fontSize: '16px',
+                  border: '1px solid #ccc',
+                  borderRadius: '4px',
+                  width: '300px'
+                }}
+              />
+              <button type="button" onClick={handleSubmitCardName}>Submit</button>
+            </div>
           ) : (
-            <button type="button" onClick={handleAddSpendingMethod}>Add Spending Method</button>
+            <div>
+              <button type="button" onClick={handleAddSpendingMethod}>Add Spending Method</button>
+                          </div>
           )}
+        </h1>
+        <h1>
+            <button type="button" onClick={handleEditSpendingData}>Edit Spending Information</button>
         </h1>
         <h1>
           <button type="button" onClick={handleSaveData}>Manual backup</button>
         </h1>
-
+        <h1>
+          <button type="button" onClick={handleDeleteAccount}>
+            {deleteConfirmation ? "Confirm Delete Account" : "Delete Account"}
+          </button>
+        </h1>
         <button type="button" onClick={toggleTheme}>Toggle Dark Mode</button>
       </form>
     </div>
